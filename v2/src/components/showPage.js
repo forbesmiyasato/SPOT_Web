3e24fcccabd252b7ba64404a700c471a82d023b9
forbesmiyasato <miya5288@pacificu.edu>
2019-10-05 20:18:31 -0700

    Displays data based on state data instead of static data (half dynamic showpage).

--- v2/src/components/showPage.js
+++ v2/src/components/showPage.js
@@ -6,10 +6,10 @@ class ShowPage extends React.Component {
         super(props);
         this.state = {
             showPopup: false, showList: [
-                { Name: "Gilbert Hall", OpenParking: 5, Distance: 10 },
-                { Name: "Gilbert Hall", OpenParking: 5, Distance: 10 },
-                { Name: "Gilbert Hall", OpenParking: 5, Distance: 10 },
-                { Name: "Gilbert Hall", OpenParking: 5, Distance: 10 }
+                { Name: "Health Profession Campus", OpenParking: 5, Distance: 10, Image: "https://upload.wikimedia.org/wikipedia/commons/6/62/Building_2_at_Pacific_University_HPC_south_side_-_Hillsboro%2C_Oregon.JPG"},
+                { Name: "Cascade Hall", OpenParking: 8, Distance: 20, Image: "https://www.walshconstruction.com/wp-content/uploads/PacU-Cascade-JoshPartee-3861-ext-corner-1450x966.jpg"},
+                { Name: "Stoller Hall", OpenParking: 5, Distance: 10, Image: "https://www.pacificu.edu/sites/html/map/images/Stoller_Center.jpg"},
+                { Name: "Gilbert Hall", OpenParking: 5, Distance: 10, Image: "https://cdnassets.hw.net/6d/51/5348993a4205be33137df8970a6b/d7b0e137-1210-40fe-beb5-36e47db92c70.jpg"}
             ]
         };
         this.togglePopup = this.togglePopup.bind(this);
@@ -33,43 +33,43 @@ class ShowPage extends React.Component {
                     <div class="row">
                         {this.state.showList.map((data) => {
                             return (
-                            <div class="col-1-of-4">
-                                <div class="card" id="popup1">
-                                    <div class="card__side card__side--front">
-                                        <div class="card__picture card__picture--3">
-                                            <img src="https://www.pacificu.edu/sites/html/map/images/Stoller_Center.jpg" />
-                                        </div>
-                                        <h4 class="card__heading">
-                                            <span class="card__heading-span">Stoller Center</span>
-                                        </h4>
-                                        <div class="card__details">
-                                            <div class="row">
-                                                <div class="col-1-of-2">
-                                                    <h3>Parkings</h3>
-                                                    <h3 class="card__details--parkings">1</h3>
-                                                </div>
-                                                <div class="col-1-of-2">
-                                                    <h3>Distance</h3>
-                                                    <h3 class="card__details--distance">5</h3>
-                                                    <h4 class="utility-center">Miles</h4>
+                                <div class="col-1-of-4">
+                                    <div class="card" id="popup1">
+                                        <div class="card__side card__side--front">
+                                            <div class="card__picture card__picture--3">
+                                                <img src={data.Image} />
+                                            </div>
+                                            <h4 class="card__heading">
+                                                <span class="card__heading-span">{data.Name}</span>
+                                            </h4>
+                                            <div class="card__details">
+                                                <div class="row">
+                                                    <div class="col-1-of-2">
+                                                        <h3>Parkings</h3>
+                                                        <h3 class="card__details--parkings">{data.OpenParking}</h3>
+                                                    </div>
+                                                    <div class="col-1-of-2">
+                                                        <h3>Distance</h3>
+                                                        <h3 class="card__details--distance">{data.Distance}</h3>
+                                                        <h4 class="utility-center">Miles</h4>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="card__side card__side--back">
-                                        <div class="card__cta">
-                                            <div id="activeBorder" class="active-border">
-                                                <div id="circle" class="circle">
-                                                    <span class="prec 270" id="prec">20%</span>
+                                        <div class="card__side card__side--back">
+                                            <div class="card__cta">
+                                                <div id="activeBorder" class="active-border">
+                                                    <div id="circle" class="circle">
+                                                        <span class="prec 270" id="prec">20%</span>
+                                                    </div>
                                                 </div>
+                                                <a href="yahoo.com" class="btn btn--white btn--animated btn__directions"> Get Directions <i class="icon-basic-geolocalize-01"></i></a>
+                                                <button onClick={this.togglePopup.bind(this)} class="btn btn--white btn--animated btn__statistics"> See Statistics <i class="icon-ecommerce-graph2"></i></button>
                                             </div>
-                                            <a href="yahoo.com" class="btn btn--white btn--animated btn__directions"> Get Directions <i class="icon-basic-geolocalize-01"></i></a>
-                                            <button onClick={this.togglePopup.bind(this)} class="btn btn--white btn--animated btn__statistics"> See Statistics <i class="icon-ecommerce-graph2"></i></button>
                                         </div>
                                     </div>
                                 </div>
-                                </div>
-                                )
+                            )
                         })
                         }
                     </div>
