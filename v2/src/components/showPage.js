5ce1531fcbfb3512a90886714df4a4cf17ea33a6
forbesmiyasato <miya5288@pacificu.edu>
2019-10-07 18:55:47 -0700

    Fixed show page grid system to support display for more than 4 parkingslots, also responsive for mobile devices now.

--- v2/src/components/showPage.js
+++ v2/src/components/showPage.js
@@ -31,7 +31,7 @@ class ShowPage extends React.Component {
                         response.data.map((data) => {
                             var distance;
                             var duration;
-                            console.log(this.state.origin.longitude);
+                            console.log(response);
                             var destinations = `${data.Lat}/${data.Lng}`;
                             var origins = `${this.state.origin.latitude}/${this.state.origin.longitude}`;
                             //console.log(process.env.REACT_APP_API);
@@ -53,18 +53,6 @@ class ShowPage extends React.Component {
                                             })
                                         })
                                 })
-
-                            //Axios.get(`http://localhost:5000/ParkingLot/${data._id}/SnapShots/latest`)
-                            //    .then(response => {
-                            //        data["OpenParkings"] = (response.data);
-                            //        data["Distance"] = distance;
-
-                            //    }).then(() => {
-                            //        this.setState({
-                            //            showList: [data, ...this.state.showList]
-                            //        })
-                            //    })
-
                         })
                     });
             },
@@ -103,10 +91,10 @@ class ShowPage extends React.Component {
                         <h2 class="section-display__text">
                             Parking lots found
             </h2>
-                        <div class="row">
+                        <div class="ui stackable four column grid">
                             {this.state.showList.map((data) => {
                                 return (
-                                    <div class="col-1-of-4">
+                                    <div class="column">
                                         <div class="card" id="popup1">
                                             <div class="card__side card__side--front">
                                                 <div class="card__picture card__picture--3">
@@ -180,124 +168,3 @@ export default ShowPage;
 //{ Name: "Cascade Hall", Lat: 41.756795, Lng: -78.954298, OpenParking: 8, Distance: 20, Image: "https://www.walshconstruction.com/wp-content/uploads/PacU-Cascade-JoshPartee-3861-ext-corner-1450x966.jpg" },
 //{ Name: "Stoller Hall", Lat: 41.756795, Lng: -78.954298, OpenParking: 5, Distance: 10, Image: "https://www.pacificu.edu/sites/html/map/images/Stoller_Center.jpg" },
 //{ Name: "Gilbert Hall", Lat: 41.756795, Lng: -78.954298, OpenParking: 5, Distance: 10, Image: "https://cdnassets.hw.net/6d/51/5348993a4205be33137df8970a6b/d7b0e137-1210-40fe-beb5-36e47db92c70.jpg" }
-            ////          <div class="col-1-of-4">
-            ////            <div class="card">
-            ////                <div class="card__side card__side--front">
-            ////                    <div class="card__picture card__picture--3">
-            ////                        <img src="https://www.walshconstruction.com/wp-content/uploads/PacU-Cascade-JoshPartee-3861-ext-corner-1450x966.jpg" />
-            ////                    </div>
-            ////                    <h4 class="card__heading">
-            ////                        <span class="card__heading-span">Cascade Hall</span>
-            ////                    </h4>
-            ////                    <div class="card__details">
-            ////                        <div class="row">
-            ////                            <div class="col-1-of-2">
-            ////                                <h3>Parkings</h3>
-            ////                                <h3 class="card__details--parkings">1</h3>
-            ////                            </div>
-            ////                            <div class="col-1-of-2">
-            ////                                <h3>Distance</h3>
-            ////                                <h3 class="card__details--distance">5</h3>
-            ////                                <h4 class="utility-center">Miles</h4>
-            ////                            </div>
-            ////                        </div>
-            ////                    </div>
-            ////                </div>
-            ////                <div class="card__side card__side--back">
-            ////                    <div class="card__cta">
-            ////                        <div id="activeBorder" class="active-border">
-            ////                            <div id="circle" class="circle">
-            ////                                <span class="prec 270" id="prec">20%</span>
-            ////                            </div>
-            ////                        </div>
-            ////                        <a href="yahoo.com" class="btn btn--white btn--animated btn__directions"> Get Directions <i class="icon-basic-geolocalize-01"></i></a>
-            ////                        <a href="google.com" class="btn btn--white btn--animated btn__statistics"> See Statistics <i class="icon-ecommerce-graph2"></i></a>
-            ////                    </div>
-            ////                </div>
-            ////            </div>
-            ////        </div>
-            ////        <div class="col-1-of-4">
-            ////            <div class="card">
-            ////                <div class="card__side card__side--front">
-            ////                    <div class="card__picture card__picture--3">
-            ////                        <img src="https://cdnassets.hw.net/6d/51/5348993a4205be33137df8970a6b/d7b0e137-1210-40fe-beb5-36e47db92c70.jpg" />
-            ////                    </div>
-            ////                    <h4 class="card__heading">
-            ////                        <span class="card__heading-span">Gilbert Hall</span>
-            ////                    </h4>
-            ////                    <div class="card__details">
-            ////                        <div class="row">
-            ////                            <div class="col-1-of-2">
-            ////                                <h3>Parkings</h3>
-            ////                                <h3 class="card__details--parkings">1</h3>
-            ////                            </div>
-            ////                            <div class="col-1-of-2">
-            ////                                <h3>Distance</h3>
-            ////                                <h3 class="card__details--distance">5</h3>
-            ////                                <h4 class="utility-center">Miles</h4>
-            ////                            </div>
-            ////                        </div>
-            ////                    </div>
-            ////                </div>
-            ////                <div class="card__side card__side--back">
-            ////                    <div class="card__cta">
-            ////                        <div id="activeBorder" class="active-border">
-            ////                            <div id="circle" class="circle">
-            ////                                <span class="prec 270" id="prec">20%</span>
-            ////                            </div>
-            ////                        </div>
-            ////                        <a href="yahoo.com" class="btn btn--white btn--animated btn__directions"> Get Directions <i class="icon-basic-geolocalize-01"></i></a>
-            ////                        <a href="google.com" class="btn btn--white btn--animated btn__statistics"> See Statistics <i class="icon-ecommerce-graph2"></i></a>
-            ////                    </div>
-            ////                </div>
-            ////            </div>
-            ////        </div>
-            ////        <div class="col-1-of-4">
-            ////            <div class="card">
-            ////                <div class="card__side card__side--front">
-            ////                    <div class="card__picture card__picture--3">
-            ////                        <img src="https://upload.wikimedia.org/wikipedia/commons/6/62/Building_2_at_Pacific_University_HPC_south_side_-_Hillsboro%2C_Oregon.JPG" />
-            ////                    </div>
-            ////                    <h4 class="card__heading">
-            ////                        <span class="card__heading-span">Health Professions Campus</span>
-            ////                    </h4>
-            ////                    <div class="card__details">
-            ////                        <div class="row">
-            ////                            <div class="col-1-of-2">
-            ////                                <h3>Parkings</h3>
-            ////                                <h3 class="card__details--parkings">1</h3>
-            ////                            </div>
-            ////                            <div class="col-1-of-2">
-            ////                                <h3>Distance</h3>
-            ////                                <h3 class="card__details--distance">5</h3>
-            ////                                <h4 class="utility-center">Miles</h4>
-            ////                            </div>
-            ////                        </div>
-            ////                    </div>
-            ////                </div>
-            ////                <div class="card__side card__side--back">
-            ////                    <div class="card__cta">
-            ////                        <div id="activeBorder" class="active-border">
-            ////                            <div id="circle" class="circle">
-            ////                                <span class="prec 270" id="prec">20%</span>
-            ////                            </div>
-            ////                        </div>
-            ////                        <a href="yahoo.com" class="btn btn--white btn--animated btn__directions"> Get Directions <i class="icon-basic-geolocalize-01"></i></a>
-            ////                        <a href="google.com" class="btn btn--white btn--animated btn__statistics"> See Statistics <i class="icon-ecommerce-graph2"></i></a>
-            ////                    </div>
-            ////                </div>
-            ////            </div>
-            ////        </div>
-            ////        </div>
-            ////    </section>
-            ////<div class="popup" id="popup">
-            ////    <div class="popup__content">
-            ////        <div class="popup__left">
-
-            ////        </div>
-            ////        <div class="popup__right">
-            ////            <a href="#popup1" class="popup__close">&times;</a>
-
-            ////        </div>
-            ////    </div>
-            ////</div>
