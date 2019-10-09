cf73d207ea0118c063c7746df5a10dd6f787fc9b
forbesmiyasato <miya5288@pacificu.edu>
2019-10-08 19:44:02 -0700

    Added local storage to get rid of undefined props on page refresh for directions page

--- v2/src/components/Map.js
+++ v2/src/components/Map.js
@@ -10,16 +10,26 @@ import {
 
 
 class Map extends Component {
+
+    constructor(props) {
+        super(props);
+        if (this.props.Destination) {
+            localStorage.setItem('DestinationLat', this.props.Destination.lat);
+            localStorage.setItem('DestinationLng', this.props.Destination.lng);
+            localStorage.setItem('OriginLat', this.props.Origin.lat);
+            localStorage.setItem('OriginLng', this.props.Origin.lng);
+        }
+    }
     state = {
         directions: null
     };
 
     componentDidMount() {
         const directionsService = new google.maps.DirectionsService();
-
-        const origin = { lat: this.props.Origin.lat, lng: this.props.Origin.lng };
-        const destination = { lat: this.props.Destination.lat, lng: this.props.Destination.lng };
-
+        console.log(localStorage.getItem('OriginLat'))
+        const origin = { lat: JSON.parse(localStorage.getItem('OriginLat')), lng: JSON.parse(localStorage.getItem('OriginLng')) };
+        const destination = { lat: JSON.parse(localStorage.getItem('DestinationLat')), lng: JSON.parse(localStorage.getItem('DestinationLng')) };
+        console.log(origin);
         directionsService.route(
             {
                 origin: origin,
@@ -37,6 +47,13 @@ class Map extends Component {
             }
         );
     }
+
+    //componentWillUpdate(nextProps, nextState) {
+    //    localStorage.setItem('DestinationLat', this.props.Destination.lat);
+    //    localStorage.setItem('DesinationLng', this.props.Destination.lng);
+    //    localStorage.setItem('OriginLat', this.props.Origin.lat);
+    //    localStorage.setItem('OriginLng', this.props.Origin.lng);
+    //}
 
     render() {
         const GoogleMapExample = withGoogleMap(props => (
