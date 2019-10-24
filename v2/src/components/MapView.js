46f714fcebd9f96ef6c157a516088b68c27e4a16
forbesmiyasato <miya5288@pacificu.edu>
2019-10-24 02:02:47 -0700

    Fixed markers rerendering on clicks

--- v2/src/components/MapView.js
+++ v2/src/components/MapView.js
@@ -2,6 +2,40 @@ import React from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
 import SidePanel from './SidePanel';
 
+class MarkersList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.markersRendered = false;
+        console.log(this.props.showList);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (JSON.stringify(this.props.showList) === JSON.stringify(nextProps.showList) && this.markersRendered) {
+            return false;
+        }
+        this.markersRendered = true;
+        return true;
+    }
+
+    render() {
+        return (
+            <span>
+                {this.props.showList.map((place, i) => {
+                    return (
+                        <Marker
+                            {...this.props}
+                            key={i}
+                            data={place}
+                            position={{ lat: place.Lat, lng: place.Lng }}
+                        />
+                    );
+                })}
+            </span>
+        )
+    }
+
+}
 class MapView extends React.Component {
     constructor(props) {
         super(props);
@@ -21,18 +55,11 @@ class MapView extends React.Component {
             showingInfoWindow: true,
             showingSidePanel: true
         });
+        //console.log(this.state.selectedPlace)
     }
 
-    //shouldComponentUpdate(nextProps, nextState) {
-    //    if (JSON.stringify(this.props.places) === JSON.stringify(nextProps.places) && this.markersRendered) {
-    //        return false;
-    //    }
-    //    this.markersRendered = true;
-    //    return true;
-    //}
-
     render() {
-        console.log(this.state.selectedPlace);
+        var selectedPlaceData = this.state.selectedPlace.data;
         var data = this.state.selectedPlace.info;
         const containerStyle = { position: 'absolute', width: '100%', height: '100%' }
 
@@ -49,30 +76,25 @@ class MapView extends React.Component {
                             lng: this.props.Origin.lng
                         }}
                     >
+                        <MarkersList showList={this.props.showList} onClick={this.onMarkerClick} />
                         <div>
                             <SidePanel Data={this.props.showList} />
                         </div>
-                        {this.props.showList.map((data) => {
-                            return <Marker
-                                onClick={this.onMarkerClick}
-                                info={data}
-                                position={{ lat: data.Lat, lng: data.Lng }}
-                            />
-                        })}
+                
 
                         <InfoWindow
                             marker={this.state.activeMarker}
                             visible={this.state.showingInfoWindow}>
                             <div>
-                                {data ?
+                                {selectedPlaceData ?
                                     (<div className="info-window">
-                                        <h2>{data.Name}</h2>
-                                        <a href={data.Image}>
-                                            <img style={{ height: '10rem', width: '10rem' }} ALIGN="right" src={data.Image} />
+                                        <h2>selectedPlaceData.Name</h2>
+                                        <a href={selectedPlaceData.Image}>
+                                            <img style={{ height: '10rem', width: '10rem' }} ALIGN="right" src={selectedPlaceData.Image} />
                                         </a>
-                                        <h3>Open Parkings: {data.OpenParkings}</h3>
-                                        <h3>Distance: <br /> {data.Distance} Miles</h3>
-                                        <h3>Duration: {data.Duration} {data.TimeUnit}</h3>
+                                        <h3>Open Parkings: {selectedPlaceData.OpenParkings}</h3>
+                                        <h3>Distance: <br /> {selectedPlaceData.Distance} Miles</h3>
+                                        <h3>Duration: {selectedPlaceData.Duration} {selectedPlaceData.TimeUnit}</h3>
                                     </div>)
                                     : null
                                 }
@@ -91,3 +113,11 @@ class MapView extends React.Component {
 export default GoogleApiWrapper({
     apiKey: "AIzaSyB-7ORj7iEWauVJmKQG6nUvEaq0unSBA9Y"
 })(MapView)
+
+        //{this.props.showList.map((data) => {
+                        //    return <Marker
+                        //        onClick={this.onMarkerClick}
+                        //        info={data}
+                        //        position={{ lat: data.Lat, lng: data.Lng }}
+                        //    />
+                        //})}
