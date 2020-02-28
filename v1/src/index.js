60a90d15acad66dfddcaf2c100457ae4176de53c
forbesmiyasato <miya5288@pacificu.edu>
2020-02-28 02:03:59 -0800

    modified file stucture for heroku deployment

--- v1/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import Router from 'react-google-maps';
-
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
